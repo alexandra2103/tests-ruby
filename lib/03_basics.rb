450915fda13f547399200f5190a61c71f9a8d1d7
alexandra2103 <pinheiro.alexandra21@gmail.com>
2021-01-18 18:36:50 +0100

    2eme envoi

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,15 +1,24 @@
 def who_is_bigger(a, b, c)
+    if a == nil || b == nil || c == nil
+        return  "nil detected"
+    elsif a > b && a > c
+        return "a is bigger"
+    elsif b > a && b > c
+        return "b is bigger"
+    elsif c > a && c > b
+        return "c is bigger"
+    end
+end
+
+
+def reverse_upcase_noLTA(text)
+    return text.reverse.upcase.tr('LTA', '')
+end
+
+
+def finder(list_42)
+    if list_42.
     return 
-end
-
-
-def crazy_stuff
-    return
-end
-
-
-def finder
-    return
 end
 
 
